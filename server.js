b26f1dab0ecbb3f42ddb81ca5b78cd3eb61c144e
Sathishramesh1 <sathishrameshkec@gmail.com>
2023-10-16 22:43:30 +0530

    second commit

--- server.js
+++ server.js
@@ -3,3 +3,21 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 require("dotenv").config();
+
+//Importing the models
+const Mentor = require("./models/Mentor");
+const Student = require("./models/Student");
+
+const app = express();
+
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+
+app.use(bodyParser.json());
+
+
+//connecting to MongoDB
+mongoose
+  .connect(DB_URL, {})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Could not connect to MongoDB", err));
